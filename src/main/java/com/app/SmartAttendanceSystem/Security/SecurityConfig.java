ec7c3642f47a208757a961ec11487137238cd1b6
Anshul Kumar Yadav <102046087+AnshulKumarYadav@users.noreply.github.com>
2026-05-30 10:02:47 +0530

    Update CORS configuration to allow all origins

--- src/main/java/com/app/SmartAttendanceSystem/Security/SecurityConfig.java
+++ src/main/java/com/app/SmartAttendanceSystem/Security/SecurityConfig.java
@@ -40,10 +40,12 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // React port
+        configuration.setAllowedOriginPatterns(Arrays.asList("*")); 
+        
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
+        
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
